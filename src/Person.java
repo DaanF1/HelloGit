844564959281157ceaae3f0cc243cb0521724600
DaanF1 <daanstander@hotmail.nl>
2024-02-02 14:07:31 +0100

    New development changes

---
+++ src/Person.java
@@ -0,0 +1,9 @@
+public class Person{
+    private String firstname;
+    private String lastname;
+
+    public Person(String firstname, String lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
